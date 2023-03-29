3b8920f924eb7939aad42e4eac7854cf98f74bb7
이수경 <leesugang@isugyeong-ui-MacBookAir.local>
2023-03-29 19:18:38 +0900

    set git ignore

---
+++ config/database.js
@@ -0,0 +1,15 @@
+const mysql = require("mysql2/promise");
+const { logger } = require("./winston");
+
+// TODO: 본인의 DB 계정 입력
+const pool = mysql.createPool({
+  host: "127.0.0.1",
+  user: "root",
+  port: "3306",
+  password: "su3081802!",
+  database: "opshop",
+});
+
+module.exports = {
+  pool: pool,
+};
